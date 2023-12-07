27c4601667c00253458b3cf0842df734f548966d
Ondřej Hlava <hlava@Ondrej--MacBook-Pro.local>
2023-12-07 15:50:40 +0100

    Add csproj file that fixes unity project not building properly

--- FireSpreadHLAVAONDREJ/FireSimulationUnity/Assets/Scripts/EventLogging.cs
+++ FireSpreadHLAVAONDREJ/FireSimulationUnity/Assets/Scripts/EventLogging.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using System.IO;
 
 // Represents a calendar for managing time within the simulation.
 public class SimulationCalendar
