c6671e60c2a7ae28ffa356cf019719022b4938f3
Ondřej Hlava <hlava@OHMacBook.local>
2024-03-14 15:55:51 +0100

    Move events to seperate folder and split into two scripts

--- FireSpreadHLAVAONDREJ/FireSimulationUnity/Assets/Scripts/Simulation/Events/EventLoggers.cs
+++ FireSpreadHLAVAONDREJ/FireSimulationUnity/Assets/Scripts/Simulation/Events/EventLoggers.cs
@@ -1,64 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-
-public enum EventType
-{
-    TileStartedBurning,
-    TileStoppedBurning,
-    WindDirectionChange,
-    WindSpeedChange
-}
-
-// Base Event class for generic event handling.
-public class Event
-{
-    public int Time { get; protected set; }
-    public EventType Type { get; protected set; }
-}
-
-// Event class specific to fire-related events.
-public class FireEvent : Event
-{
-    public Tile Tile { get; private set; }
-
-    public FireEvent(int time, EventType type, Tile tile)
-    {
-        Time = time;
-        Type = type;
-        Tile = tile;
-    }
-}
-
-// Event class specific for weather-related changes in the simulation.
-public class WindEvent : Event
-{
-    public int OldWindDirection { get; private set; }
-    public int NewWindDirection { get; private set; }
-    public float OldWindSpeed { get; private set; }
-    public float NewWindSpeed { get; private set; }
-
-    // Constructor for wind direction changes
-    public WindEvent(int time, EventType type, int oldWindDirection, int newWindDirection)
-    {
-        Time = time;
-        Type = type;
-        OldWindDirection = oldWindDirection;
-        NewWindDirection = newWindDirection;
-    }
-
-    // Constructor for wind speed changes
-    public WindEvent(int time, EventType type, float oldWindSpeed, float newWindSpeed)
-    {
-        Time = time;
-        Type = type;
-        OldWindSpeed = oldWindSpeed;
-        NewWindSpeed = newWindSpeed;
-    }
-}
-
-
-
 
 // Unified Event Logger
 public class EventLogger<T> where T : Event
